86ccf5703e3eec5d41749a031597d064b463574d
Michela <micheladellagatta1@gmail.com>
2025-11-07 12:17:46 +0100

    Create Header macro component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 import faqs from "./data/languages.js";
 
 function App() {
   return (
     <>
+      <Header />
       <Main items={faqs} />
     </>
   );
