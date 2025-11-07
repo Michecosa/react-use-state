f8c9c1b0b3956877e6d1a41d232fa296755bb193
Michela <micheladellagatta1@gmail.com>
2025-11-07 11:48:55 +0100

    Display buttons in page

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -4,6 +4,12 @@ export default function FAQ({ item }) {
   return (
     <div>
       <h2>Learn Web development</h2>
+
+      {item.map((element) => (
+        <button key={element.id} className="btn btn-primary mx-2">
+          {element.title}
+        </button>
+      ))}
     </div>
   );
 }
