0f64a942141fed37adaec08e75cb46f69723d16d
Michela <micheladellagatta1@gmail.com>
2025-11-07 12:01:37 +0100

    Add useState to handle opening the description of the selected button

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
+
 export default function FAQ({ item }) {
-  console.log("FAQ renderizzato", item);
+  const [open, setAperta] = useState(null);
+
+  function toggleFaq(id) {
+    setAperta(open === id ? null : id);
+  }
 
   return (
     <div>
-      <h2>Learn Web development</h2>
-      <div className="d-flex">
+      <h2>Learn Web Development</h2>
+
+      <div className="d-flex gap-2">
         {item.map((element) => (
-          <button key={element.id} className="btn btn-primary mx-2">
-            {element.title}
-          </button>
+          <div key={element.id} className="mb-3">
+            <button
+              className={`mb-4 btn ${
+                open === element.id ? "btn-warning" : "btn-primary"
+              }`}
+              onClick={() => toggleFaq(element.id)}
+            >
+              {element.title}
+            </button>
+            {open === element.id && (
+              <div className="card p-3 mt-2">
+                <h5>{element.title}</h5>
+                <div>{element.description}</div>
+              </div>
+            )}
+          </div>
         ))}
       </div>
     </div>
