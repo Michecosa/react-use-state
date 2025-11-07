c7bacac035c12249e69676aa106fea9551b3cc5e
Michela <micheladellagatta1@gmail.com>
2025-11-07 12:57:45 +0100

    Before selection, display default message

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,19 @@
 export default function Card({ items, open }) {
   return (
-    <>
-      {items.map(
-        (item) =>
-          open === item.id && (
-            <div key={item.id} className="card p-3 mt-2">
-              {" "}
-              <h4>{item.title}</h4> <p className="mb-0">{item.description}</p>{" "}
-            </div>
-          )
+    <div className="card p-3 mt-2">
+      {open === null ? (
+        <p className="mb-0 text-center">Nessun linguaggio selezionato</p>
+      ) : (
+        items.map(
+          (item) =>
+            open === item.id && (
+              <>
+                <h4 key={item.id}>{item.title}</h4>
+                <p className="mb-0">{item.description}</p>
+              </>
+            )
+        )
       )}
-    </>
+    </div>
   );
 }
