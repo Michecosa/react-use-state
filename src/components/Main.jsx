35c98831e2316918169e0d3e35ce449d68de0194
Michela <micheladellagatta1@gmail.com>
2025-11-07 12:18:02 +0100

    Wrap component with container class

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,9 @@ import FAQ from "./FAQ.jsx";
 export default function Main({ items }) {
   return (
     <>
-      <FAQ items={items} />
+      <div class="container mt-5">
+        <FAQ items={items} />
+      </div>
     </>
   );
 }
